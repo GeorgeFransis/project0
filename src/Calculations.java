70dae1a7f3966a0c28e3348b23f019bb4b00bbcb
George Emad <georgeemad@Georges-MacBook-Air.local>
2021-08-30 22:24:44 -0700

    added a comment to the source file

--- src/Calculations.java
+++ src/Calculations.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Calculations
 {
+	//This is the first project of CS1400
 	public static void main(String [] args)
 	{
 		Scanner keyboard = new Scanner(System.in);
